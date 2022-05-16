2cf344339a283c309dc3ee1fe7833bdb2ffa532b
yanglh14 <yanglh14@163.com>
2022-05-16 21:01:01 +0800

    add corl22 task

---
+++ isaacgymenvs/tasks/CoRL2022.py
@@ -0,0 +1,91 @@
+from isaacgym import gymapi
+
+
+gym = gymapi.acquire_gym()
+
+# get default set of parameters
+sim_params = gymapi.SimParams()
+
+# set common parameters
+sim_params.dt = 1 / 60
+sim_params.substeps = 1
+sim_params.up_axis = gymapi.UP_AXIS_Z
+sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+
+# set PhysX-specific parameters
+sim_params.physx.use_gpu = True
+sim_params.physx.solver_type = 1
+sim_params.physx.num_position_iterations = 6
+sim_params.physx.num_velocity_iterations = 1
+sim_params.physx.contact_offset = 0.01
+sim_params.physx.rest_offset = 0.0
+
+sim = gym.create_sim(0, 0, gymapi.SIM_PHYSX, sim_params)
+
+gym.prepare_sim(sim)
+# configure the ground plane
+plane_params = gymapi.PlaneParams()
+plane_params.normal = gymapi.Vec3(0, 0, 1) # z-up!
+plane_params.distance = 0
+plane_params.static_friction = 1
+plane_params.dynamic_friction = 1
+plane_params.restitution = 0
+
+# create the ground plane
+gym.add_ground(sim, plane_params)
+
+#### load desk asset
+asset_root = "../../assets"
+asset_file = "tactile/CoRL2022/corl2022.xml"
+asset_options = gymapi.AssetOptions()
+asset_options.fix_base_link = True
+asset_options.armature = 0.01
+
+asset1 = gym.load_asset(sim, asset_root, asset_file, asset_options)
+
+#### load object asset
+asset_root = "../../assets"
+asset_file = "tactile/objects/cube.xml"
+asset_options = gymapi.AssetOptions()
+
+asset2 = gym.load_asset(sim, asset_root, asset_file, asset_options)
+
+# set up the env grid
+num_envs = 1
+envs_per_row = 8
+env_spacing = 2.0
+env_lower = gymapi.Vec3(-env_spacing, 0.0, -env_spacing)
+env_upper = gymapi.Vec3(env_spacing, env_spacing, env_spacing)
+
+
+# create and populate the environments
+for i in range(num_envs):
+    env = gym.create_env(sim, env_lower, env_upper, envs_per_row)
+
+    pose = gymapi.Transform()
+    pose.p = gymapi.Vec3(0.1, 0.1, 0.1)
+    actor_handle = gym.create_actor(env, asset1, pose, "Desk", i, 1)
+
+    pose = gymapi.Transform()
+    pose.p = gymapi.Vec3(0.1, 0.1, 0.2)
+    actor_handle = gym.create_actor(env, asset2, pose, "Object", i, 1)
+
+cam_props = gymapi.CameraProperties()
+viewer = gym.create_viewer(sim, cam_props)
+# cam_pos = gymapi.Vec3(0, 0, 1)
+# cam_target = gymapi.Vec3(0, 0, 0)
+# gym.viewer_camera_look_at(viewer, None, cam_pos, cam_target)
+
+while not gym.query_viewer_has_closed(viewer):
+
+    # step the physics
+    gym.simulate(sim)
+    gym.fetch_results(sim, True)
+
+    # update the viewer
+    gym.step_graphics(sim);
+    gym.draw_viewer(viewer, sim, True)
+
+    # Wait for dt to elapse in real time.
+    # This synchronizes the physics simulation with the rendering rate.
+    gym.sync_frame_time(sim)
